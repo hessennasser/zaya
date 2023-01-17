29f17df083f9c72263f68a833f709ea4e7582ce0
Hessen Nasser <92742551+hessennasser@users.noreply.github.com>
2023-01-18 01:08:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// start menu in header 
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav");
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("active");
+    nav.classList.toggle("active");
+});
+// end menu in header
+
+// start btn-up 
+btnUp = document.querySelector('.up-btn');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 250) {
+        btnUp.classList.add("active");
+    } else {
+        btnUp.classList.remove("active");
+    }
+});
+btnUp.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+// end btn-up 
+// start btn-up 
+whatsBtn = document.querySelector('.whats-btn');
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
+        whatsBtn.classList.add("active");
+    } else {
+        whatsBtn.classList.remove("active");
+    }
+});
+// end btn-up
