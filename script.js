f33dcb7c6916e92196af8857f1ed4a954da96f10
Hessen Nasser <92742551+hessennasser@users.noreply.github.com>
2023-01-18 13:27:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -34,3 +34,24 @@ window.addEventListener('scroll', function () {
     }
 });
 // end btn-up
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3000, // delay in milliseconds
+    },
+});
